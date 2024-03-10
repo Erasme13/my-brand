5a25fce5d9a7b7cfaf428ff6d84fc87132685026
Hoziyana Erasme <hozerasme2@gmail.com>
2024-03-10 19:32:22 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// My dropdowns
+document.querySelector('.dropdwncontainer').addEventListener ('mouseover', function (){
+    document.getElementById('dropdown').style.display = 'block';
+});
+document.querySelector('.dropdwncontainer').addEventListener('mouseout', function() {
+    document.getElementById('dropdown').style.display = 'none';
+});
+// my dropdowns for smaller screens
+document.querySelector('.dropdwncontainer2').addEventListener ('mouseover', function() {
+    document.getElementById('dropdown2').style.display = 'block';
+});
+document.querySelector('.dropdwncontainer2').addEventListener ('mouseout', function() {
+    document.getElementById('dropdown2').style.display ='none'
+})
+
+// Toggle menu bar 
+var togglebtn = document.querySelector('.toggle-btn');
+var mymenu = document.querySelector('.mymenu');
+togglebtn.addEventListener('click', () => {
+    mymenu.classList.toggle('active');
+})
