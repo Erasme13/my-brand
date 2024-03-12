009df4d8af15ed5563bb819c728b98f9cfe16b10
Hoziyana Erasme <hozerasme2@gmail.com>
2024-03-12 13:56:13 +0200

    added new features

--- admin.js
+++ admin.js
@@ -11,3 +11,22 @@ function updatePerformanceMetrics() {
 updatePerformanceMetrics();
 
 // Recent activity
+var activities = [
+    {type: "project", name: "New project added"},
+    {type: "blog", name: "new blog post: How to use flex-box"},
+    {type: "project", name: "Project Z was added"}
+];
+function generateActivityFeed() {
+    var activityList = document.getElementById('activity-list');
+    activityList.innerHTML = '';
+
+activities.forEach(function(activity) {
+    listItem = document.createElement('li');
+    listItem.classList.add('activity-item');
+    var activityText = document.createElement('p');
+    activityText.textContent = activity.name;
+    listItem.appendChild(activityText);
+    activityList.appendChild(listItem);
+});
+}
+generateActivityFeed ();
