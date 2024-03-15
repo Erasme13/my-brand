8d01baa0b2f98969bd5400a4bbc2de889f2e20e7
Hoziyana Erasme <hozerasme2@gmail.com>
2024-03-15 13:36:39 +0200

    added new features

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
         event.preventDefault();
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
-        var rememberMe = document.getElementById('remember-me').value;
+        var rememberMe = document.getElementById('remember-me').checked;
         if (rememberMe) {
             localStorage.setItem('username', username);
             localStorage.setItem('password', password);
@@ -14,16 +14,22 @@ document.addEventListener('DOMContentLoaded', function(){
             localStorage.removeItem('password');
 
         }
-        
-        var storedUsers = JSON.parse(localStorage.getItem('users')) || [];
-        var userFound = storedUsers.find(function(user) {
-            return user.username == username && user.password == password;
-        })
+        var storedUsers =[];
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const userData = JSON.parse(localStorage.getItem(key));
+            if (userData && userData.username && userData.password) {
+                storedUsers.push(userData);
+            }
+        }
+        var userFound = storedUsers.find(function(user){
+            return user.username === username && user.password === password;
+        });
         if (userFound) {
             alert ('Login successful');
             window.location.href = "index.html";
         } else {
-            alert('inavlid username or password! please try again.');
+            alert('invalid username or password! please try again.');
             document.getElementById('username').value = "";
             document.getElementById('password').value = "";
             document.getElementById('username').focus();
--- signup.js
+++ signup.js
@@ -1,25 +1,108 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mySignupForm = document.getElementById('mysignup-form');
+document.addEventListener('DOMContentLoaded', function(){
 
-mySignupForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    var username =document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var rememberMe = document.getElementById('remember-me').checked;
-    if(rememberMe) {
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
-    } else{
-        localStorage.removeItem('username');
-        localStorage.removeItem('password');
+    let myForm = document.getElementById('mysignup-form');
+    let username = document.getElementById('username');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let password2 = document.getElementById('password2');
+
+    
+    const setError = (el, message) => {
+        const inputControl = el.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success');
+    
     }
-    if(localStorage.getItem(username)) {
-        alert('This account already exists');
+    const setSuccess = el => {
+        const inputControl = el.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+        
+    }
+    const validEmail = (e) => {
+        const regex = /\w+@\w+.\w+/;
+        return regex.test(String(e).toLowerCase());
+    
+    };
+    const validPassword = (p) => {
+        const regex = /^(?=.*\p{Ll})(?=.*\p{Lu})(?=.*[\d|@#$!%*?&])[\p{L}\d@#$!%*?&]{8,96}$/gmu;;
+        return regex.test(p);
+    }
+    const validUser = (u) => {
+        const re = /[^0-9]/;
+        return re.test(u);
+    }
+    const validateInputs = (usernameValue, emailValue, passwordValue, password2Value) => {
+        let isValid = true;
+
+    if (localStorage.getItem(usernameValue)) {
+        setError(username, "Username is already taken")
+        isValid = false; 
+    }
+    if (localStorage.getItem(emailValue)){
+        setError(email, "This email is already registered");
+        isValid = false;
+    }
+    
+    if (usernameValue === '') {
+        setError(username, "Username is required please!");
+        isValid = false;
+    }else if (!validUser(usernameValue)) {
+        setError(username, "digits are not allowed")
+        isValid = false;
+    }else {
+        setSuccess(username)
+    }
+
+    if (emailValue === '') {
+        setError(email, "email is required");
+        isValid = false;
+    }else if(!validEmail(emailValue)) {
+        setError(email, "provide valid email address please");
+        isValid = false;
     } else {
-        localStorage.setItem(username, JSON.stringify({password: password}));
-        alert('signup completed! please login')
-        window.location.href = "login page.html";
+        setSuccess(email);
     }
-    mySignupForm.reset();
-});
+    if (passwordValue ==='') {
+        setError(password, "Password is required");
+        isValid = false;
+    } else if (!validPassword(passwordValue)) {
+        setError (password, "Password must contain at least one lowercase letter, one uppercase letter, one number, one symbol, and be at least 8 characters long")
+        isValid = false;
+    }else {
+        setSuccess(password);
+    }
+    if (password2Value ==='') {
+        setError(password2, "Comfirm your password please");
+        isValid = false;
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords don't match");
+        isValid = false;
+    } else {
+        setSuccess(password2);
+    }
+    return isValid;
+};
+
+        myForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+        const usernameValue = username.value.trim();
+        const emailValue = email.value.trim();
+        const passwordValue = password.value.trim();
+        const password2Value = password2.value.trim();
+           if (validateInputs(usernameValue, emailValue, passwordValue, password2Value)) {
+        const userData = {username: usernameValue, email: emailValue, password: passwordValue};
+              localStorage.setItem(usernameValue, JSON.stringify(userData));
+              localStorage.setItem(emailValue, JSON.stringify(userData));
+              alert ("Signup completed! pleaase login")
+              window.location.href = "login page.html";
+              myForm.reset();
+           }
+        
+        });
+    
 });
